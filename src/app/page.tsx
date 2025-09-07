e8eb658bb5fb4c43d25361ebf67633dce84be31d
Deepak Kumar <deepaktechk@gmail.com>
2025-09-07 13:25:24 +0530

    Shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-        <div>
+        <div className="text-3xl p-4">
+            <p>Hello, world</p>
 
+            <Button variant="destructive">Click me</Button>
         </div>
     );
 }
